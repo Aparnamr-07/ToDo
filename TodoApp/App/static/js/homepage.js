9e933d9a7b67d93ec6ca3e097c6d01b98ed9fbba
Aparna M R <aparnamr07@gmail.com>
2025-07-23 13:30:54 +0530

    Banner Completed

--- TodoApp/App/static/js/homepage.js
+++ TodoApp/App/static/js/homepage.js
@@ -1,7 +1,12 @@
 function closebtn(){
     document.querySelector(".reg-wrapper").style.display="none"
 }
-
 function regoff(){
     document.querySelector(".reg-wrapper").style.display="flex"
 }
+function closeicon(){
+    document.querySelector(".loginwrapper").style.display="none"
+}
+function loginoff(){
+    document.querySelector(".loginwrapper").style.display="flex"
+} 
