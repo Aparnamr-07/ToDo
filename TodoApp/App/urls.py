a76e3bb6793e17a01f8ce1e0fbb407a839d58457
Aparna M R <aparnamr07@gmail.com>
2025-07-28 14:13:31 +0530

    commit

--- TodoApp/App/urls.py
+++ TodoApp/App/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from . import views 
 urlpatterns = [
     path('',views.homepage,name="homepage"), 
-    path('task/',views.task,name="task"),
     path('edit/',views.edit,name="edit"),
     path('login/',views.loginpage,name="login"),
+    path('signup/',views.signup,name="signup"),
     path('logout/',views.userlogout,name="logout"),
-    
-]
+    path('addtodo/',views.addtodo,name="addtodo"),
+    path('profileedit/<int:pk>',views.profileedit,name="profileedit"),
+    path('edittodo/<int:pk>',views.edittodo,name="edittodo"),
+    path('todocomplete/<int:pk>',views.todocomplete,name="todocomplete"),
+    path('trash/<int:pk>',views.trash,name="trash"),
+    path('tododelete/<int:pk>',views.tododelete,name="tododelete"),
+] 
